6b40124cf6fa3933e50c8778a7432047c217057e
fchinita98 <fchinita98@gmail.com>
2025-07-10 11:01:53 +0100

    create court

--- backend/services/courts.js
+++ backend/services/courts.js
@@ -1,19 +1,30 @@
 const { insertCourt, findCourt, updateCourt, deleteCourt } = require('../data/courts')
 
-const errors = {
-    message: "The data is invalid",
-    error: "This court Number already exists"
-}
-
 async function createCourt (data) {
 
-    const { status, courtNumber, queue } = data
+    const { courtNumber, courtLevel } = data
+    let status = true;
+    let queue = []
 
      // confirmação se court já existe
     const user = await findCourt({courtNumber})
     if (user && courtNumber === user.courtNumber) {
-       return {message: errors.message, error: errors.error}
+       return {message: "Court number already exists."}
     }
-
-    status = true;
+    // confirmação de que o tipo de dados courtNumber é bem inserido
+    if (!courtNumber || typeof courtNumber !== 'number') {
+        return { message: "Invalid court number" }
+    }
+    // confirmação de que o tipo de dados courtLevel é bem inserido
+    if (courtLevel && typeof courtLevel !== 'string') {
+        return { message: "Invalid court level" }
+    }
+    // Enviar os seguintes dados para a DB
+    const courtData = { status, courtNumber, queue, courtLevel }
+    // se passar todas as confirmações, executa a função
+    const id = await insertCourt(courtData);
+    
+    return { message: "Court created", id }
 }
+
+module.exports = { createCourt }
