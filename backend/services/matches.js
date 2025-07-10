9f5690afdd22db6dc3dae9d5bc2f6163c01fc93e
fchinita98 <fchinita98@gmail.com>
2025-07-10 17:05:15 +0100

    match commit

--- backend/services/matches.js
+++ backend/services/matches.js
@@ -33,7 +33,7 @@ async function startMatch(court) {
     return matchId
 }
 
-async function finishTeam (matchId, winningTeam) {
+async function finishMatch (matchId, winningTeam) {
     // find match por ID
     const match = await findMatch({ _id: new ObjectId(String(matchId)) })
     if (!match || match.status !== "In Progress") {
@@ -46,11 +46,47 @@ async function finishTeam (matchId, winningTeam) {
     // Update in the DB
     await updateMatch({_id: matchId}, match);
 
-    if (winningTeam === teamA) {
+    // Iniciar variaveis de vitoriosos e derrotados
+    let losers = null;
+    let winners = null;
 
+    // atribuição de winningTeam
+    if (winningTeam === "teamA") {
+        losers = match.teamB;
+        winners = match.teamA;
+    } else if (winningTeam !== "teamA") {
+        losers = match.teamB;
+        winners = match.teamA;
+    } else {
+        throw new Error("Invalid winning team.");
     }
-    const losers = winningTeam === teamA ? match.teamB : match
+
+    // Se queue não tiver +2 jogadores para jogar
+    if (court.queue.length < 2) {
+        return { message: "Match finished. Waiting for more players..."}
+    }
+
+    // ir buscar os 2 próximos jogadores
+    const player1 = court.queue[0];
+    const player2 = court.queue[1];
+    court.queue = court.queue.slice(2);
+
+    // novo jogo com os vencedores do passado
+    const newMatch = {
+        courtId: court._id,
+        teamA: winners,
+        teamB: [player1, player2],
+        status: "In Progress",
+        winningTeam: null,
+        started: new Date(),
+        finished: null
+    }
+    // criar novo jogo com os novos dados
+    await insertMatch(newMatch)
+
+    // Atualizar court com nova queue
+    await updateCourt({ _id: court._id }, { queue: court.queue });
 }
 
-module.exports = { startMatch, }
+module.exports = { startMatch, finishMatch }
 
