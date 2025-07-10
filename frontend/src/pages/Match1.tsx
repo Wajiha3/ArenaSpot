7e5677f1676e65c67537b10b2d63679a3938d86b
Wajiha3 <wajihaaziz195@gmail.com>
2025-07-10 09:14:15 +0100

    project pages

--- frontend/src/pages/Match1.tsx
+++ frontend/src/pages/Match1.tsx
@@ -18,19 +18,19 @@ function Match1() {
       </div>
 
       {/* Match title */}
-      <h1 className="text-2xl font-bold mb-16 mt-11 text-center">Match 1</h1>
+      <h1 className="mb-16 text-[2rem] font-bold mt-[2rem] text-center text-white">Match 1</h1>
 
       {/* Teams section */}
       <div className="flex justify-between mb-6 gap-2">
         {/* Team 1 */}
-        <div className="text-center w-[48%] bg-gray-100 rounded-lg">
+        <div className="text-center w-[48%] bg-white rounded-lg">
           <div className="p-4 rounded-lg mt-3 mb-3 flex flex-col items-center">
             <p className="text-2xl font-bold text-black">Joao</p>
             <p className="text-2xl font-bold text-black">Goncalo</p>
           </div>
         </div>
         {/* Team 2 */}
-        <div className="text-center w-[48%] bg-gray-100 rounded-lg">
+        <div className="text-center w-[48%] bg-white rounded-lg">
           <div className="p-4 rounded-lg mb-3 flex flex-col items-center">
             <div className="flex items-center gap-2 mt-4">
               <div className="flex flex-col items-start">
