d14d9e64eb706bccb7f84859ed9236e5e9836f84
fchinita98 <fchinita98@gmail.com>
2025-07-11 11:41:44 +0100

    done Matches

--- backend/services/matches.js
+++ backend/services/matches.js
@@ -1,6 +1,7 @@
 const { insertMatch, findMatch, findAllMatches, updateMatch, deleteMatch } = require('../data/matches')
 const { insertCourt, findCourt, findAllCourts, updateCourt, deleteCourt } = require('../data/courts')
 const { ObjectId } = require('mongodb')
+const { findUser, updateUser } = require('../data/user')
 
 async function startMatch(court) {
     // se partida tiver menos de 4 elementos na queue
@@ -61,6 +62,29 @@ async function finishMatch (matchId, winningTeam) {
         throw new Error("Invalid winning team.");
     }
 
+    // Buscar os dados dos users vencedores e derrotados
+    const winner1 = await findUser({ _id: new ObjectId(String(winners[0])) });
+    const winner2 = await findUser({ _id: new ObjectId(String(winners[1])) });
+    const loser1 = await findUser({ _id: new ObjectId(String(losers[0])) });
+    const loser2 = await findUser({ _id: new ObjectId(String(losers[1])) });
+
+    const winnerUsers = [winner1, winner2];
+    const loserUsers = [loser1, loser2];
+
+    // Calcula média dos pontos por equipa
+    const avgWinnersPoints = (winner1.points + winner2.points) / 2
+    const avgLosersPoints = (loser1.points + loser2.points) / 2
+
+    // Atualizar pontos vencedores
+    for (const winner of winnerUsers) {
+        await updateUserStats(winner._id, true, avgWinnersPoints, avgLosersPoints);
+    }
+
+    // Atualizar pontos derrotados
+    for (const loser of loserUsers) {
+        await updateUserStats(loser._id, false, avgLosersPoints, avgWinnersPoints)
+    }
+
     // Atualiza match:
     match.status = "Finished";
     match.winningTeam = winningTeam;
@@ -100,5 +124,40 @@ async function finishMatch (matchId, winningTeam) {
     return { message: "New match started.", match: newMatch }
 }
 
-module.exports = { startMatch, finishMatch }
 
+async function updateUserStats (userId, userWin, teamAvg, opponentAvg) {
+    const user = await findUser({ _id: new ObjectId(String(userId))})
+    if (!user) return
+
+    // pontos de participação
+    let pointsGained = 3; 
+
+    const updateProperties = {
+        gamesPlayed: (user.gamesPlayed + 1),
+        wins: user.wins,
+        losses: user.losses
+    }
+
+    if (userWin === true) {
+        pointsGained += 5;
+        updateProperties.wins += 1
+    }
+
+    if (userWin === false) {
+        updateProperties.losses += 1
+    }
+
+    if (userWin === true && teamAvg < opponentAvg) {
+        pointsGained += 2;
+    }
+
+    await updateUser (
+        { _id: user._id },
+        { points: (user.points + pointsGained),
+            ...updateProperties
+        }
+    )
+}
+
+
+module.exports = { startMatch, finishMatch, updateUserStats }
